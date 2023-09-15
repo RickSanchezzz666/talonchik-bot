b6d849591f453bd266a5c4576bef360e844f0d63
RickSanchezzz666 <lapiyktube@gmail.com>
2023-09-15 13:15:42 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+import time
+import pygame
+
+driver = webdriver.Chrome()
+
+pygame.mixer.init()
+sound = pygame.mixer.Sound('alert.mp3')
+sound.set_volume(1.0)
+
+driver.get('https://eq.hsc.gov.ua/site/step2?chdate=2023-09-16&question_id=55&id_es=')
+
+
+time.sleep(40)
+
+while True:
+    try:
+        err_elem = driver.find_element(By.XPATH, "//*[contains(text(), 'nginx')]")
+        driver.refresh()
+        time.sleep(3) 
+    except NoSuchElementException:
+        pass
+    time.sleep(3)
+    first_location = driver.find_element('css selector', "img[style*='z-index: 194']")
+    first_location.click()
+    time.sleep(2)
+
+    try:
+        second_location = driver.find_element('css selector', '#submit')
+        sound.play()
+        print('Талончик знайдено!')
+        input('Нажми Enter для завершення...')
+        break
+    except NoSuchElementException:
+        driver.refresh()
+        time.sleep(3)
